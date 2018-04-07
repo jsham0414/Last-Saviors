cbfc7a0784a4a52d3d74892fd0e5c7efc8012e72
jsham0414 <jsham0414@naver.com>
2018-04-08 00:28:45 +0900

    UI & 받을 패킷 추가

---
+++ Assets/UI/Dialog/Dialog.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialog : MonoBehaviour {
+
+    void Start() {
+        Destroy(gameObject, 5.0f);
+    }
+
+    public void AcceptClick() {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/UI/RoomUI/RoomUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomUI : MonoBehaviour {
+	void Start () {
+		foreach (Text t in GetComponentsInChildren<Text>()) {
+            switch (t.name) {
+            case "RoomTitle":
+                t.text = DataManager.Instance.NowRoom.Name;
+                break;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
