9bdf9bd5709650df7cce435d17cae70734462601
jsham0414 <jsham0414@naver.com>
2018-04-07 21:15:11 +0900

    데드 레커닝 가속도 삭제

--- Assets/Character/NetPlayer/NetPlayer.cs
+++ Assets/Character/NetPlayer/NetPlayer.cs
@@ -41,20 +41,20 @@ public class NetPlayer : Character {
 
     void DeadReconing() {
         Vector3 dr = new Vector3(0, 0, 0);
-        float dx = dPosition.x + Info.Speed * ((Time.time - deadTime)) + (0.5f * (Time.time - deadTime) * (Time.time - deadTime));
-        float dy = dPosition.y + Info.Speed * ((Time.time - deadTime)) + (0.5f * (Time.time - deadTime) * (Time.time - deadTime));
+        float dx = dPosition.x + Info.Speed * ((Time.time - deadTime));
+        float dy = dPosition.y + Info.Speed * ((Time.time - deadTime));
 
         if (dx * deadX == 0)
             dr.x = dPosition.x;
         else if (deadX < 0)
-            dr.x = dPosition.x - Info.Speed * ((Time.time - deadTime)) + (0.5f * (Time.time - deadTime) * (Time.time - deadTime));
+            dr.x = dPosition.x - Info.Speed * ((Time.time - deadTime));
         else
             dr.x = dx * deadX;
 
         if (dy * deadY == 0)
             dr.y = dPosition.y;
         else if (deadY < 0)
-            dr.y = dPosition.y - Info.Speed * ((Time.time - deadTime)) + (0.5f * (Time.time - deadTime) * (Time.time - deadTime));
+            dr.y = dPosition.y - Info.Speed * ((Time.time - deadTime));
         else
             dr.y = dy * deadY;
 
